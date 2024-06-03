cc29b5acedd446905e44fa6b4bfddee714a178e3
quangtrandinhminh <trandinhminhquangsxp@gmail.com>
2024-06-03 14:33:27 +0700

    config for deploy CI/CD

--- src/PetHealthCareSystemAPI/Program.cs
+++ src/PetHealthCareSystemAPI/Program.cs
@@ -21,10 +21,15 @@ var app = builder.Build();
 app.UseMiddleware<ErrorHandlerMiddleware>();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "PetHealthCareSystemAPI");
+        options.RoutePrefix = string.Empty;
+        options.DocumentTitle = "PetHealthCareSystemAPI";
+    });
 }
 
 app.UseHttpsRedirection();
