df7a4e74e398197defc4c7115b6e2741bd3d43d1
quangtrandinhminh <trandinhminhquangsxp@gmail.com>
2024-06-04 03:19:48 +0700

    Update database
    Change all key from string to int
    Change vetid to timetableId in appoinment and hospitalization
    Add new migration

--- src/BusinessObject/Entities/TimeTable.cs
+++ src/BusinessObject/Entities/TimeTable.cs
@@ -1,11 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using BusinessObject.Entities.Base;
 
 namespace BusinessObject.Entities;
 
 [Table("TimeTable")]
 public class TimeTable : BaseEntity
 {
-    public string VetID { get; set; }
+    public int VetID { get; set; }
     public DateTimeOffset DateTimeStart { get; set; }
     public DateTimeOffset DateTimeEnd { get; set; }
     public ICollection<DayOfWeek> DayOfWeeks { get; set; }
@@ -13,4 +14,7 @@ public class TimeTable : BaseEntity
     
     [ForeignKey(nameof(VetID))]
     public virtual User Vet { get; set; }
+    
+    public virtual ICollection<Hospitalization>? Hospitalizations { get; set; }
+    public virtual ICollection<Appointment>? Appointments { get; set; }
 }
