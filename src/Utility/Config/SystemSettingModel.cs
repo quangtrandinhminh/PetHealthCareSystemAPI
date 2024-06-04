df00ae0aa833590cf9887865e6c4f28245e462c8
quangtrandinhminh <trandinhminhquangsxp@gmail.com>
2024-06-05 01:19:19 +0700

    config JWT

---
+++ src/Utility/Config/SystemSettingModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+
+namespace Utility.Config
+{
+    public class SystemSettingModel
+    {
+        public static SystemSettingModel Instance { get; set; }
+
+        public static IConfiguration Configs { get; set; }
+        public string ApplicationName { get; set; } = Assembly.GetEntryAssembly()?.GetName().Name;
+
+        public string? Domain { get; set; }
+        public string SecretKey { get; set; }
+        public string SecretCode { get; set; }
+    }
+
+    public class MailSettingModel
+    {
+        public static MailSettingModel Instance { get; set; }
+        public SmtpSetting Smtp { get; set; }
+        public string FromAddress { get; set; }
+        public string FromDisplayName { get; set; }
+    }
+
+    public class SmtpSetting
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public bool EnableSsl { get; set; }
+        public bool UsingCredential { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
