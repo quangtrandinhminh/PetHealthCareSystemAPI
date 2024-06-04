e5f62691b67b148e564df91710e72af14d5506d3
quangtrandinhminh <trandinhminhquangsxp@gmail.com>
2024-06-05 01:19:35 +0700

    Add Mapper

---
+++ src/BusinessObject/Mapper/Mapper.cs
@@ -0,0 +1,12 @@
+﻿using Riok.Mapperly.Abstractions;
+
+namespace BusinessObject.Mapper;
+
+[Mapper]
+public partial class MapperlyMapper
+{
+    //  public partial entity Map(dto request); --for create
+    // public partial dtoresponse Map(entity entity); --for get
+    // public partial IList<dtoresponse> Map(IList<entity> entity); --for get all
+    // public partial void Map(dto request, entity entity); --for update
+}
--- src/PetHealthCareSystemAPI/Program.cs
+++ src/PetHealthCareSystemAPI/Program.cs
@@ -1,24 +1,77 @@
 using System.Reflection;
 using BusinessObject.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using PetHealthCareSystemAPI.Middlewares;
 using Repository;
 using Serilog;
+using Utility.Config;
 
 var builder = WebApplication.CreateBuilder(args);
+
 // Add services to the container.
 
-// add serilog and get configuration from appsettings.json
+// Add CORS policy
+const string myAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Configuration.AddUserSecrets(Assembly.GetExecutingAssembly(), true);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(myAllowSpecificOrigins,
+        policy =>
+        {
+            policy
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowAnyOrigin();
+            //.AllowCredentials();
+                                      
+        });
+});
+
+// Add serilog and get configuration from appsettings.json
 builder.Services.AddSerilog(config => { config.ReadFrom.Configuration(builder.Configuration); });
+
+// Add DbContext and ConnectionString
 builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Configuration.GetSection("SystemSetting").Bind(SystemSettingModel.Instance);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(o =>
+{
+    o.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Name = "AddAuthorization",
+        Description = "Give me bearer token to call API here!",
+        Type = SecuritySchemeType.Http
+    });
+    o.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Id = "Bearer",
+                    Type = ReferenceType.SecurityScheme
+                }
+            },
+            new List<string>()
+        }
+    });
+    o.SwaggerDoc("v1", new OpenApiInfo { Title = "PetHealthCareSystem", Version = "v1" });
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    o.IncludeXmlComments(System.IO.Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 var app = builder.Build();
+app.UseCors(myAllowSpecificOrigins);
 app.UseMiddleware<ErrorHandlerMiddleware>();
 
 // Configure the HTTP request pipeline.
