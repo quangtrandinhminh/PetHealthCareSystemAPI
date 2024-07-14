5cacf55afd7954c4049e280d51aec11d3690aa1f
quangtrandinhminh <trandinhminhquangsxp@gmail.com>
2024-07-15 03:37:39 +0700

    Update HospitalizationPriceResponseDto.cs

--- src/BusinessObject/DTO/Transaction/HospitalizationPriceResponseDto.cs
+++ src/BusinessObject/DTO/Transaction/HospitalizationPriceResponseDto.cs
@@ -7,7 +7,7 @@ public class HospitalizationPriceResponseDto
     public int MedicalRecordId { get; set; }
     public decimal PricePerDay { get; set; }
     public int Days { get; set; }
-    public DateTimeOffset AdmissionDate { get; set; }
-    public DateTimeOffset DischargeDate { get; set; }
+    public DateTimeOffset? AdmissionDate { get; set; }
+    public DateTimeOffset? DischargeDate { get; set; }
     public decimal TotalPrice { get; set; }
 }
